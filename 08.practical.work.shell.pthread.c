9394fd3de4e1a42967fc4daad44bd588665dabf7
Bui Quang Huy <huybui@Huys-MacBook-Pro.local>
2021-03-17 18:23:04 +0700

    change 4 spaces into 1 tab

--- 08.practical.work.shell.pthread.c
+++ 08.practical.work.shell.pthread.c
@@ -8,45 +8,45 @@
 int childpid;
 char cmd[1000];
 void handler(int signal_num) {
-    printf("Signal %d => ", signal_num);
-    switch (signal_num) {
-        case SIGTSTP:
-            printf("pause\n");
-            kill(childpid,SIGTSTP);
-            break;
-        case SIGINT:
-        case SIGTERM:
-            printf("Terminated\n");
-            exit(0);
-            break;
-    }
+	printf("Signal %d => ", signal_num);
+	switch (signal_num) {
+		case SIGTSTP:
+			printf("pause\n");
+			kill(childpid,SIGTSTP);
+			break;
+		case SIGINT:
+		case SIGTERM:
+			printf("Terminated\n");
+			exit(0);
+			break;
+	}
 }
 
 void *backgoundTask(){
-    int pid = fork();
-    if(pid == 0) { // this is child who execute shell
-        char* args[] = {"/bin/bash", "-c", cmd, NULL};
-        execvp(args[0], args);
-    } 
-    else { // this is parent who wait for next comment
-        childpid = pid;
-        wait(&pid);
-    }
+	int pid = fork();
+	if(pid == 0) { // this is child who execute shell
+		char* args[] = {"/bin/bash", "-c", cmd, NULL};
+		execvp(args[0], args);
+	} 
+	else { // this is parent who wait for next comment
+		childpid = pid;
+		wait(&pid);
+	}
 }
 
 int main() {
-    // ctrl z
-    signal(SIGTSTP, handler);
-    signal(SIGINT, handler);
-    signal(SIGTERM, handler);
+	// ctrl z
+	signal(SIGTSTP, handler);
+	signal(SIGINT, handler);
+	signal(SIGTERM, handler);
 	while(1){
 		printf("$ ");
-        memset(cmd,0,sizeof(cmd));
+		memset(cmd,0,sizeof(cmd));
 		fgets(cmd, sizeof(cmd), stdin);
 		if (strcmp(cmd,"/q\n") == 0) break;
 		pthread_t tid;
-        pthread_create(&tid, NULL, backgoundTask, NULL);
-        pthread_join(tid, NULL);
+		pthread_create(&tid, NULL, backgoundTask, NULL);
+		pthread_join(tid, NULL);
 	}
 	return 0;
 }
